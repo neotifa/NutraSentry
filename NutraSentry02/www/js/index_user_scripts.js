e382e83da7f0ce2202286ab329374871c229b45b
Erica Boyer <boyer.373@osu.edu>
2014-11-15 11:03:34 -0500

    Added more stuff

---
+++ NutraSentry02/www/js/index_user_scripts.js
@@ -0,0 +1,41 @@
+(function()
+{
+ "use strict";
+    
+    var userName = ""
+    
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  bViewProfile */
+    $(document).on("click", "#bViewProfile", function(evt)
+    {
+         activate_subpage("#user_profile"); 
+    });
+    
+        /* button  uib_w_4 */
+    $(document).on("click", ".uib_w_4", function(evt)
+    {
+         activate_subpage("#new_user_signup"); 
+    });
+    
+        /* button  bSubmitNewUser */
+    $(document).on("click", "#bSubmitNewUser", function(evt)
+    {
+         activate_subpage("#user_profile"); 
+    });
+    
+        /* button  bLogin */
+    $(document).on("click", "#bLogin", function(evt)
+    {
+        userName = ddUserName.get();
+         activate_subpage("#user_profile"); 
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
